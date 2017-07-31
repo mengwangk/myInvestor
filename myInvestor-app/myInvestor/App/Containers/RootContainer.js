8f9f34444671185082ae5c34c77aec8fb4c2634b
mengwangk <mengwangk@gmail.com>
2017-07-31 20:01:24 +0800

    Modified files

--- myInvestor-app/myInvestor/App/Containers/RootContainer.js
+++ myInvestor-app/myInvestor/App/Containers/RootContainer.js
@@ -1,34 +1,34 @@
-import React, { Component } from 'react'
-import { View, StatusBar } from 'react-native'
-import ReduxNavigation from '../Navigation/ReduxNavigation'
-import { connect } from 'react-redux'
-import StartupActions from '../Redux/StartupRedux'
-import ReduxPersist from '../Config/ReduxPersist'
+import React, { Component } from "react";
+import { View, StatusBar } from "react-native";
+import ReduxNavigation from "../Navigation/ReduxNavigation";
+import { connect } from "react-redux";
+import StartupActions from "../Redux/StartupRedux";
+import ReduxPersist from "../Config/ReduxPersist";
 
 // Styles
-import styles from './Styles/RootContainerStyles'
+import styles from "./Styles/RootContainerStyles";
 
 class RootContainer extends Component {
-  componentDidMount () {
+  componentDidMount() {
     // if redux persist is not active fire startup action
     if (!ReduxPersist.active) {
-      this.props.startup()
+      this.props.startup();
     }
   }
 
-  render () {
+  render() {
     return (
       <View style={styles.applicationView}>
-        <StatusBar barStyle='light-content' />
+        <StatusBar barStyle="light-content" />
         <ReduxNavigation />
       </View>
-    )
+    );
   }
 }
 
 // wraps dispatch to create nicer functions to call within our component
-const mapDispatchToProps = (dispatch) => ({
+const mapDispatchToProps = dispatch => ({
   startup: () => dispatch(StartupActions.startup())
-})
+});
 
-export default connect(null, mapDispatchToProps)(RootContainer)
+export default connect(null, mapDispatchToProps)(RootContainer);
--- myInvestor-app/myInvestor/App/Navigation/AppNavigation.js
+++ myInvestor-app/myInvestor/App/Navigation/AppNavigation.js
@@ -1,18 +1,22 @@
-import { StackNavigator } from "react-navigation";
-import HomeScreen from "../Containers/HomeScreen";
-import WatchlistScreen from "../Containers/WatchlistScreen";
-import SettingsScreen from "../Containers/SettingsScreen";
-import LaunchScreen from "../Containers/LaunchScreen";
+import { DrawerNavigator, StackNavigator } from 'react-navigation';
+import HomeScreen from '../Containers/HomeScreen';
+import WatchlistScreen from '../Containers/WatchlistScreen';
+import SettingsScreen from '../Containers/SettingsScreen';
+import LaunchScreen from '../Containers/LaunchScreen';
+import SampleContainer from '../Containers/SampleContainer';
+import SampleScreen from '../Containers/SampleScreen';
 
 import styles from "./Styles/NavigationStyles";
 
 // Manifest of possible screens
-const PrimaryNav = StackNavigator(
+const PrimaryNav = DrawerNavigator(
   {
     HomeScreen: { screen: HomeScreen },
     WatchlistScreen: { screen: WatchlistScreen },
     SettingsScreen: { screen: SettingsScreen },
-    LaunchScreen: { screen: LaunchScreen }
+    LaunchScreen: { screen: LaunchScreen },
+    SampleContainer: { screen: SampleContainer },
+    SampleScreen: { screen: SampleScreen }
   },
   {
     // Default config for all screens
