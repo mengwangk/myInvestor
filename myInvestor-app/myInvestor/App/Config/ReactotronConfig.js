a43885221df43802b7d66101ca2b50f94e71e298
Koh Meng Wang <mengwangk@gmail.com>
2017-10-24 00:07:59 +0800

    Fixed Reactotron

--- myInvestor-app/myInvestor/App/Config/ReactotronConfig.js
+++ myInvestor-app/myInvestor/App/Config/ReactotronConfig.js
@@ -11,15 +11,23 @@ import { reactotronRedux as reduxPlugin } from "reactotron-redux";
 import sagaPlugin from "reactotron-redux-saga";
 
 if (Config.useReactotron) {
-  
+  console.log("connecting to reactotron");
   // https://github.com/infinitered/reactotron/issues/431
   // https://github.com/infinitered/reactotron for more options!
-  //Reactotron.configure({  host: 'localhost', name: "myInvestor" })
+  // https://github.com/infinitered/reactotron/issues/162g
+  // Reactotron.configure({  host: 'localhost', name: "myInvestor" })
+  // Do adb reverse tcp:9090 tcp:9090
   Reactotron
     .useReactNative()
     .use(reduxPlugin({ onRestore: Immutable }))
     .use(sagaPlugin())
-    .connect();
+    .connect(
+      { 
+        enabled: true,
+        host: 'localhost',
+        port:9090
+      }
+    );
 
   // Let's clear Reactotron on every time we load the app
   Reactotron.clear();
