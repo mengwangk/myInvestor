4c4d644474461e31e380142a09e690798d5879ed
Koh Meng Wang <mengwangk@gmail.com>
2017-12-05 23:34:42 +0800

    Removed unnecessary ios files

--- myInvestor-app/ios/Pods/Target Support Files/Pods-myInvestor/Pods-myInvestor-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_myInvestor : NSObject
-@end
-@implementation PodsDummy_Pods_myInvestor
-@end
--- myInvestor-app/ios/Pods/Target Support Files/Pods-myInvestorTests/Pods-myInvestorTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_myInvestorTests : NSObject
-@end
-@implementation PodsDummy_Pods_myInvestorTests
-@end
